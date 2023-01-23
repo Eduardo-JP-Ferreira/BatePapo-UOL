c4c5c847a8ab1a8493223b9f62d960ef407fce49
Eduardo Ferreira <eduardopires2014@gmail.com>
2023-01-23 11:46:44 -0300

    Aprimoramento do Código

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let guardaMensagem={};
 entrada();
 function entrada(){
     nomeUsuario = prompt("Digite o nome do Usuário:");
-    
+
     usuario =
     {
         name: `${nomeUsuario}`
@@ -14,12 +14,11 @@ function entrada(){
     const request = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', usuario);
     request.then(iniciaSistema);
     request.catch(erroGeral);
-    
 }
-function iniciaSistema(resposta){
-    buscarMensagens()
-    let ConfirmaUsuarioOnline = setInterval(informaServidor, 5000);
-    let AtualizarChat = setInterval(buscarMensagens, 3000);
+function iniciaSistema(){
+    buscarMensagens();
+    const ConfirmaUsuarioOnline = setInterval(informaServidor, 5000);
+    const AtualizarChat = setInterval(buscarMensagens, 3000);
 }
 function informaServidor(){
     const informaNomeParaServidor = axios.post('https://mock-api.driven.com.br/api/v6/uol/status',usuario);
@@ -34,7 +33,7 @@ function usuarioNaoOnline(){
     console.log("Usuário Offline");
     entrada();
 }
-let input = document.getElementById("input");
+const input = document.getElementById("input");
 input.addEventListener("keyup", function(event) {
      if (event.keyCode === 13) {
         event.preventDefault();
@@ -57,7 +56,6 @@ function enviarMensagem(){
 function envioRealizado(){
     document.querySelector('.campoMensagem').value='';
     buscarMensagens();
-    
 }
 function buscarMensagens(){
     const respostaMensagemEnviada = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
@@ -70,9 +68,9 @@ function processarResposta(resposta) {
     const listaMensagens = document.querySelector('ul');
     listaMensagens.innerHTML = '';
     for(let i=0; i<guardaMensagem.length; i++){
-        if(guardaMensagem[i].type == "private_message"&&(guardaMensagem[i].from == usuario.name || guardaMensagem[i].to == usuario.name)){
+        if(guardaMensagem[i].type === "private_message"&&(guardaMensagem[i].from === usuario.name || guardaMensagem[i].to === usuario.name)){
             if(i==99){
-                let template = `
+                const template = `
                 <li data-test="message" class="ultimo private">
                     <span>(${guardaMensagem[i].time})</span>&nbsp; <b>${guardaMensagem[i].from}</b>&nbsp; para &nbsp;<b>${guardaMensagem[i].to}</b>: ${guardaMensagem[i].text}
                 </li>
@@ -80,7 +78,7 @@ function processarResposta(resposta) {
                 listaMensagens.innerHTML += template; 
             }
             else{
-                let template = `
+                const template = `
                 <li data-test="message" class="private">
                     <span>(${guardaMensagem[i].time })</span>&nbsp; <b>${guardaMensagem[i].from }</b>&nbsp; para &nbsp;<b>${guardaMensagem[i].to}</b>: ${guardaMensagem[i].text}
                 </li>
@@ -88,9 +86,9 @@ function processarResposta(resposta) {
                 listaMensagens.innerHTML += template;
             }
         }
-        else if(guardaMensagem[i].type == "status"){
+        else if(guardaMensagem[i].type === "status"){
             if(i==99){
-                let template = `
+                const template = `
                 <li data-test="message" class="ultimo status">
                     <span>(${guardaMensagem[i].time})</span>&nbsp; <b>${guardaMensagem[i].from}</b>&nbsp; ${guardaMensagem[i].text}
                 </li>
@@ -98,7 +96,7 @@ function processarResposta(resposta) {
                 listaMensagens.innerHTML += template; 
             }
             else{
-                let template = `
+                const template = `
                 <li data-test="message" class="status">
                     <span>(${guardaMensagem[i].time })</span>&nbsp; <b>${guardaMensagem[i].from}</b>&nbsp; ${guardaMensagem[i].text}
                 </li>
@@ -107,9 +105,9 @@ function processarResposta(resposta) {
             }
 
         }
-        else if(guardaMensagem[i].type == "message"){
+        else if(guardaMensagem[i].type === "message"){
             if(i==99){
-                let template = `
+                const template = `
                 <li data-test="message" class="ultimo msg">
                     <span>(${guardaMensagem[i].time})</span>&nbsp; <b>${guardaMensagem[i].from}</b>&nbsp; para &nbsp;<b>${guardaMensagem[i].to}</b>: ${guardaMensagem[i].text}
                 </li>
@@ -117,7 +115,7 @@ function processarResposta(resposta) {
                 listaMensagens.innerHTML += template; 
             }
             else{
-                let template = `
+                const template = `
                 <li data-test="message" class="msg">
                     <span>(${guardaMensagem[i].time })</span>&nbsp; <b>${guardaMensagem[i].from}</b>&nbsp; para &nbsp;<b>${guardaMensagem[i].to}</b>: ${guardaMensagem[i].text}
                 </li>
